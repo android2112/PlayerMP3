d14cd7ec0297c64d44559e5bcbfcf0e4f17abafc
MARCO BUONFIGLIO <you@example.com>
2018-07-25 17:26:25 +0200

    add interface

--- app/src/main/java/com/example/marco/playermp3/Activity/MainActivity.java
+++ app/src/main/java/com/example/marco/playermp3/Activity/MainActivity.java
@@ -4,14 +4,24 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.example.marco.playermp3.Adapter.Track;
+import com.example.marco.playermp3.Adapter.TrackAdapter;
 import com.example.marco.playermp3.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+List<Track>tracklist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +39,28 @@ public class MainActivity extends AppCompatActivity {
             }
         });*/
 
+        tracklist=new ArrayList<>();
 
+        tracklist.add(new Track(321,"Alba",3.16,"Venditti","fddfgfjh"));
+        tracklist.add(new Track(584,"Acqua",2.5,"Mina","jhkjhjkh"));
+
+setupRecyclerView();
+
+
+    }
+
+    public void setupRecyclerView(){
+        RecyclerView recyclerview=findViewById(R.id.recyclerView);
+        TrackAdapter adapter=new TrackAdapter(tracklist, new TrackAdapter.OnItemListener() {
+            @Override
+            public void setItemClickListener(int position, Track track, View view) {
+                Toast.makeText(MainActivity.this, "position:"+position, Toast.LENGTH_SHORT).show();
+            }
+        });
+        LinearLayoutManager layoutManager=new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false);
+
+        recyclerview.setLayoutManager(layoutManager);
+        recyclerview.setAdapter(adapter);
     }
 
 
--- app/src/main/java/com/example/marco/playermp3/Adapter/TrackAdapter.java
+++ app/src/main/java/com/example/marco/playermp3/Adapter/TrackAdapter.java
@@ -5,6 +5,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -16,50 +17,75 @@ import java.util.List;
 
 public class TrackAdapter extends RecyclerView.Adapter<TrackAdapter.ViewHolder> {
 
-    public interface onItemListener{
+    public interface OnItemListener {
         void setItemClickListener(int position, Track track, View view);
     }
 
-    private List<Track>tracklist;
-    private onItemListener onItemListaner;
+    private List<Track> trackList;
+    private OnItemListener onItemListener;
+    //private ViewHolder holder;
 
-    public TrackAdapter(List<Track> tracklist, onItemListener onItemListaner) {
-        this.tracklist = tracklist;
-        this.onItemListaner = onItemListaner;
+    public TrackAdapter(List<Track> trackList, OnItemListener onItemListener) {
+        this.trackList = trackList;
+        this.onItemListener = onItemListener;
     }
 
     @NonNull
     @Override
-    public TrackAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-
-        return null;
-        }
-
-    @Override
-    public void onBindViewHolder(@NonNull TrackAdapter.ViewHolder viewHolder, int i) {
-
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_row, parent, false);
+        return new ViewHolder(v);
     }
 
+    @Override
+    public void onBindViewHolder(@NonNull final ViewHolder holder, final int position) {
+
+        //this.holder = holder;
+
+        final Track currentTrack = trackList.get(position);
+
+        holder.title.setText(currentTrack.getTitle());
+        holder.artist.setText(currentTrack.getArtist());
+        holder.duration.setText(String.valueOf(currentTrack.getDuration()));
+
+        holder.touch_row.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (onItemListener != null) {
+                    onItemListener.setItemClickListener(position, currentTrack, holder.touch_row);
+                }
+            }
+        });
+    }
+/*
+    public void isPlaying() {
+        holder.isPlaying.setVisibility(View.VISIBLE);
+        holder.isPlaying.setImageResource(R.drawable.mp3logo);
+    }
+*/
     @Override
     public int getItemCount() {
-        return tracklist.size();
+        return trackList.size();
     }
+
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
         private TextView title;
         private TextView duration;
         private TextView artist;
-        private RelativeLayout touchrow;
+        private RelativeLayout touch_row;
+        private ImageView isPlaying;
 
-
-        public ViewHolder(@NonNull View itemView) {
+        public ViewHolder(View itemView) {
             super(itemView);
 
-            title=itemView.findViewById(R.id.title);
-            duration=itemView.findViewById(R.id.duration);
-            artist=itemView.findViewById(R.id.artist);
-            touchrow=itemView.findViewById(R.id.touchrow);
+            title = itemView.findViewById(R.id.title);
+            duration = itemView.findViewById(R.id.duration);
+            artist = itemView.findViewById(R.id.artist);
+            touch_row = itemView.findViewById(R.id.touchrow);
+            //isPlaying = itemView.findViewById(R.id.isPlay);
+
         }
     }
 }
